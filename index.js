5055c24c26d7cf683e6a9ba181727e70cd279b35
syck24 <bastianabarca280@gmail.com>
2023-07-20 17:40:52 -0400

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const app = require("./app")
+const app = require("./app");
 const {
   DB_USER,
   DB_PASSWORD,
@@ -10,23 +10,25 @@ const {
 
 const PORT = process.env.POST || 3977;
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`)
-.then((respuesta) =>{
-    console.log("Conexión Exitante!")
-    app.listen(PORT, () =>{
-        console.log("##################")
-        console.log("#### API REST ####")
-        console.log("##################")
-        console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}`)
-    })
-}).catch((error) =>{
-    console.log(error)
-});
+mongoose
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${a}/`)
+  .then((respuesta) => {
+    console.log("Conexión Exitante!");
+    app.listen(PORT, () => {
+      console.log("##################");
+      console.log("#### API REST ####");
+      console.log("##################");
+      console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // const connectDB = async () => {
 
 //   try {
-//     await 
+//     await
 //     mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`)
 //     console.log('Conexión Exitante');
 //     app.listen(PORT, () => {
@@ -40,5 +42,5 @@ mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`)
 //     console.log('Error al conectar con la base de datos', error);
 //   }
 // }
-  
+
 // connectDB()
