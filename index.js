e041c2a2b5521d8947482d92cfb959822ff87553
syck24 <bastianabarca280@gmail.com>
2023-07-20 18:25:35 -0400

    start c

--- index.js
+++ index.js
@@ -11,23 +11,20 @@ const {
 // Use PORT provided in environment or default to 3000
 const port = process.env.PORT || 3000;
 
-// Listen on `port` and 0.0.0.0
-app.listen(port, "0.0.0.0", function () {
-  mongoose
-    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`)
-    .then((respuesta) => {
-      console.log("Conexión Exitante!");
-      app.listen(PORT, () => {
-        console.log("##################");
-        console.log("#### API REST ####");
-        console.log("##################");
-        console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}`);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
+mongoose
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`)
+  .then((respuesta) => {
+    console.log("Conexión Exitante!");
+    app.listen(port, "0.0.0.0", function () {
+      console.log("##################");
+      console.log("#### API REST ####");
+      console.log("##################");
+      console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}`);
     });
-});
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // const connectDB = async () => {
 
